df41bb9d3c2886fa93b93b29324e7b51dd330166
fakesociety <211634151+rupcgroup25-2@users.noreply.github.com>
2025-06-06 23:42:36 +0300

    Update user class

---
+++ Newsite-Server/BL/User.cs
@@ -0,0 +1,30 @@
+﻿namespace Newsite_Server.BL
+{
+    public class User
+    {
+        int id;
+        string name;
+        string email;
+        string password;
+        bool active;
+        bool blockSharing;
+
+        public User() { }
+        public User(int id, string name, string email, string password, bool active, bool blockSharing)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.Active = active;
+            this.BlockSharing = blockSharing;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public bool Active { get => active; set => active = value; }
+        public bool BlockSharing { get => blockSharing; set => blockSharing = value; }
+    }
+}
---
+++ Newsite-Server/DAL/DBservices.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Data.SqlClient;
+using Newsite_Server.BL;
+namespace Newsite_Server.DAL
+{
+    public class DBservices
+    {
+        public DBservices()
+        {
+       
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // This method creates a connection to the database according to the connectionString name in the appsettings.json 
+        //--------------------------------------------------------------------------------------------------
+        public SqlConnection connect(String conString)
+        {
+
+            // read the connection string from the configuration file
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json").Build();
+            string cStr = configuration.GetConnectionString("myProjDB");
+            SqlConnection con = new SqlConnection(cStr);
+            con.Open();
+            return con;
+        }
+
+        //פונקציות
+
+
+
+        private SqlCommand CreateCommandWithStoredProcedureGeneral(String spName, SqlConnection con, Dictionary<string, object> paramDic)
+        {
+
+            SqlCommand cmd = new SqlCommand(); // create the command object
+
+            cmd.Connection = con;              // assign the connection to the command object
+
+            cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+            cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+            if (paramDic != null)
+                foreach (KeyValuePair<string, object> param in paramDic)
+                {
+                    cmd.Parameters.AddWithValue(param.Key, param.Value);
+
+                }
+
+
+            return cmd;
+        }
+    }
+}

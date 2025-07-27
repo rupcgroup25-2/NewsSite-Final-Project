0c62a5fd668a2a3a3f0201a3d41659837bcceaa6
nivb852 <36416860+nivb852@users.noreply.github.com>
2025-07-27 13:09:32 +0300

    Removing events.js, indexArticleActions

--- client/news-moty/js/shared/events.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).on('click', '[data-category]', function () {
-    const cat = $(this).data('category');
-    renderArticles(cat);
-});
-
-
-$('a[data-bs-toggle="tab"]').on('shown.bs.tab', function () {
-    renderTabs();
-});

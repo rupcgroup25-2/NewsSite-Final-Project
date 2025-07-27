063c1a32db8cd5a07de38bf23562f1ed24c4a0d3
nivb852 <36416860+nivb852@users.noreply.github.com>
2025-07-27 13:35:35 +0300

    convert loading of theme to a script

---
+++ client/news-moty/js/shared/loadThemeImmediatly.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    const savedTheme = localStorage.getItem('newsHubTheme') || 'light';
+    document.documentElement.setAttribute('data-bs-theme', savedTheme);
+    if (document.body) {
+        document.body.setAttribute('data-bs-theme', savedTheme);
+    }
+})();
